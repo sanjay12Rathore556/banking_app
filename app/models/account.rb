c8cdcb8b473b275df7d1eeb081f8702f70586ffc
sanjay12Rathore556 <sanjayrathore5566@gmail.com>
2018-10-05 05:07:17 +0530

    update callbacks

--- app/models/account.rb
+++ app/models/account.rb
@@ -4,4 +4,11 @@ class Account < ApplicationRecord
   validates :balance,:account_type,presence: true
   validates :account_no,numericality: {only_integer: true},uniqueness: true
   validates :account_type, inclusion: { in: %w(saving current)}
+  validate :check_valid_balance
+end
+private
+  def check_valid_balance
+    if self.balance < 1000
+      errors.add(:base, "For create an account your balance should be grater than 1000")
+    end
 end
